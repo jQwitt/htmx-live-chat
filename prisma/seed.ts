584f9e3ae7854d5cbe7913d3e1acdfb5c45c30c6
Jack Witt <Jwitt1452@gmail.com>
2023-07-24 17:12:06 -0700

    feat: update schema and seed control

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,9 +3,27 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+    await prisma.chat.create({
+        data: {
+            title: 'Tester',
+            description: 'A test chat',
+        },
+        select: {
+            participants: true,
+        },
+    });
     await prisma.user.create({
         data: {
             name: 'Jack',
+            avatar: 'null',
+            chats: {
+                create: [
+                    {
+                        title: "Jack's Room",
+                        description: 'A personal room just for Jack',
+                    },
+                ],
+            },
         },
         select: {
             id: true,
