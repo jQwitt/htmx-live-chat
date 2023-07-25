3d63695e6826983af10e91d2a3f52cfba9271444
Jack Witt <Jwitt1452@gmail.com>
2023-07-24 17:13:48 -0700

    feat: update tailwind, css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
     content: ['./src/**/*.{tsx, html}'],
     theme: {
+        colors: {
+            text: '#0f0505',
+            background: '#fafafa',
+            primary: '#19d253',
+            secondary: '#d9f2e1',
+            accent: '#7f19e6',
+        },
         extend: {},
     },
     plugins: [],
